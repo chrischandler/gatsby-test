c17172125432d8b8fb5b7f7567f7af4d17993323
Chris Chandler <chris@chrischandler.com>
2020-06-26 15:30:15 +0800

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
